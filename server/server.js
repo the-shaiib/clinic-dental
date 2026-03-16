db73e14229113b08ca4683b05eb03a70b2b44099
𝒕𝒉𝒆𝗦𝗵aii𝗯 <zakaryachaib4@gmail.com>
2026-03-16 20:35:43 +0100

    up

--- server/server.js
+++ server/server.js
@@ -12,11 +12,12 @@ const app = express();
 
 const allowedOrigins = process.env.CLIENT_ORIGINS
   ? process.env.CLIENT_ORIGINS.split(',').map((origin) => origin.trim().replace(/\/$/, ''))
-  : ['http://localhost:5173'];
+  : null;
 
 const corsOptions = {
   origin: (origin, callback) => {
     if (!origin) return callback(null, true);
+    if (!allowedOrigins || allowedOrigins.length === 0) return callback(null, true);
     const normalizedOrigin = origin.replace(/\/$/, '');
     if (allowedOrigins.includes(normalizedOrigin)) return callback(null, true);
     return callback(new Error('The CORS policy for this site does not allow access from the specified Origin.'), false);
